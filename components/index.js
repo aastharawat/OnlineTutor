963a4c707afddc0271da1b73ba949dd930fa2879
unknown <aastha.rawat33@gmail.com>
2020-08-16 23:37:38 +0530

    remove bklot

--- components/index.js
+++ components/index.js
@@ -1,7 +1,5 @@
 const mongoose = require("mongoose");
 require("dotenv").config();
-const session = require("express-session");
-const MongoStore = require("connect-mongo")(session);
 
 mongoose.connect(
   process.env.DB_STRING,
